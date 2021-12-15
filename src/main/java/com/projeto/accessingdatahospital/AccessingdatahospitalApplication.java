dc6365692a261023075266a4865b9c4637fb2a80
paulovictorpv25 <paulovictornlds@hotmail.com.br>
2021-12-15 19:22:51 -0300

    Projeto Clínico Hospital HRT

---
+++ src/main/java/com/projeto/accessingdatahospital/AccessingdatahospitalApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.accessingdatahospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingdatahospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingdatahospitalApplication.class, args);
+	}
+
+}

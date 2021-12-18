61a1c15c42cf4281854dd434170118601517de09
paulovictorpv25 <paulovictornlds@hotmail.com.br>
2021-12-18 03:05:00 -0300

    Correcao , conexao com tela gerenciador

--- src/main/java/com/projeto/accessingdatahospital/MvcConfig.java
+++ src/main/java/com/projeto/accessingdatahospital/MvcConfig.java
@@ -8,11 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/home").setViewName("home");
-		registry.addViewController("/").setViewName("home");
-		registry.addViewController("/hello").setViewName("hello");
 		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
 		registry.addViewController("/manager").setViewName("manager");
+		registry.addViewController("/gerenciar").setViewName("gerenciar");
 		registry.addViewController("/css/signin.css").setViewName("css/signin.css");
 		registry.addViewController("/img/governobrasilia.png").setViewName("img/governobrasilia.png");
 	}
